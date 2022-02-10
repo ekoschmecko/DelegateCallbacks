2c0fd341287ba7ddc44eca58e03459b2cf181640
EkoSchmecko <60213794+ekoschmecko@users.noreply.github.com>
2022-02-10 10:45:39 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿
+namespace WindowsFormsApp21
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonDock = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonDock
+            // 
+            this.buttonDock.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonDock.Location = new System.Drawing.Point(0, 0);
+            this.buttonDock.Name = "buttonDock";
+            this.buttonDock.Size = new System.Drawing.Size(334, 221);
+            this.buttonDock.TabIndex = 0;
+            this.buttonDock.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(334, 221);
+            this.Controls.Add(this.buttonDock);
+            this.MaximumSize = new System.Drawing.Size(350, 260);
+            this.MinimumSize = new System.Drawing.Size(0, 260);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonDock;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp21
+{
+    public partial class Form1 : Form
+    {
+        int width = 0;
+
+        public delegate void Del();
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.ResizeEnd += Form1_ResizeEnd;
+            width = this.Size.Width;           
+        }
+
+        private void Form1_ResizeEnd(object sender, EventArgs e)
+        {
+            Del op;
+
+            if(width >= buttonDock.Size.Width)
+            {
+                op = new Del(FormSmaller);
+            }
+            else if(width <= buttonDock.Size.Width)
+            {
+                op = new Del(FormBigger);
+            }
+            else
+            {
+                op = new Del(Unknown);
+            }
+
+            op();
+
+            width = buttonDock.Size.Width;
+        }
+
+        private void FormSmaller()
+        {
+            MessageBox.Show("Kleiner");
+        }
+
+        private void FormBigger()
+        {
+            MessageBox.Show("Größer");
+        }
+
+        private void Unknown()
+        {
+            MessageBox.Show("Unbekannt");
+        }
+
+    }
+}
